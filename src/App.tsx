990a6256575482e1f8b5f1db69a41cf9d217284a
agussc <aguss360@gmail.com>
2024-02-04 19:13:23 -0300

    update vite version

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./pages";
-import { APIProvider } from "@vis.gl/react-google-maps";
-
-import "atropos/css";
-import "swiper/css";
+// import { APIProvider } from "@vis.gl/react-google-maps";
 
 const App = () => {
-  const apiKey = import.meta.env.VITE_MAPS_API_KEY;
+  // const apiKey = import.meta.env.VITE_MAPS_API_KEY;
 
   return (
-    <APIProvider apiKey={apiKey}>
-      <RouterProvider router={router} />
-    </APIProvider>
+    // <APIProvider apiKey={apiKey}>
+    <RouterProvider router={router} />
+    // </APIProvider>
   );
 };
 export default App;
